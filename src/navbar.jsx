a9b8546abdaced5d4a97468a7d87d86e72e92854
Software Engineer 24 <Software.Engineer24@bafokeng.local>
2024-06-15 18:29:04 +0200

    created a navbar and added a favicon

---
+++ src/navbar.jsx
@@ -0,0 +1,14 @@
+export function Navbar (){
+    return  <nav className="nav">
+     <a href="/"  className="site-title">podcast</a>
+     <ul>
+        <li>
+        <a href="/">Shows</a>
+        </li>
+        <li>
+        <a href="/">LogIn</a>
+        </li>
+        </ul> 
+    </nav>
+    
+ }
