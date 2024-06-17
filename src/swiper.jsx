24601715ea540b714d23b753cfbbddb466133acb
SABELOMAWELA <mawelas63@gmail.com>
2024-06-17 06:58:43 +0200

    "Added Swiper modules and updated Swiper component props"

--- src/swiper.jsx
+++ src/swiper.jsx
@@ -1,11 +1,18 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, A11y } from 'swiper/modules';
 import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 
 const SwiperComponent = () => {
   return (
     <Swiper
+      modules={[Navigation, Pagination, A11y]}
       spaceBetween={50}
-      slidesPerView={3}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}
       onSlideChange={() => console.log('slide change')}
       onSwiper={(swiper) => console.log(swiper)}
     >
